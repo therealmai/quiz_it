ca85864b11fd413c7b213986b12dd3d615868051
Jomar Leaño <69794504+therealmai@users.noreply.github.com>
2022-11-30 19:59:53 +0800

    ep 18

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,31 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer Chosen');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What is your favorite color?',
+      'What\'s your favorite animal'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('QuizHub'),
+          title: const Text('QuizHub'),
         ),
-        body: Text('This is my default text'),
+        body: Column(
+          children: [
+            const Text('The Question'),
+            ElevatedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+            ElevatedButton(child: Text('Answer 2'), onPressed: answerQuestion),
+            ElevatedButton(
+                child: Text('Answer 3'),
+                onPressed: () => print('Answer 3 chosen')),
+          ],
+        ),
       ),
     );
   }
