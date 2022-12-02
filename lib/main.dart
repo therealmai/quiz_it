f209acb94ae49fd4701531921be7bf86f4f33ec0
Jomar Leaño <69794504+therealmai@users.noreply.github.com>
2022-12-02 16:40:33 +0800

    review map ep 30

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
+import './answer.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,8 +23,18 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'What is your favorite color?',
-      'What\'s your favorite animal?'
+      {
+        'questionText': 'What is your favorite color?',
+        'answers': ['Black', 'Red', 'Green', 'White'],
+      },
+      {
+        'questionText': 'What\'s your favorite animal?',
+        'answers': ['Rabbit', 'Lion', 'Owl']
+      },
+      {
+        'questionText': 'What\'s your favorite game?',
+        'answers': ['Hades', 'Dota', 'Witcher']
+      },
     ];
     return MaterialApp(
       home: Scaffold(
@@ -32,10 +43,12 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Question(questions[_questionIndex]),
-            ElevatedButton(child: Text('Answer 1'), onPressed: _answerQuestion),
-            ElevatedButton(child: Text('Answer 2'), onPressed: _answerQuestion),
-            ElevatedButton(child: Text('Answer 3'), onPressed: _answerQuestion),
+            Question(
+              questions[_questionIndex]['questionText'] as String,
+            ),
+            ...(questions[_questionIndex]['answers'] as List<String>).map((answer){
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
       ),
