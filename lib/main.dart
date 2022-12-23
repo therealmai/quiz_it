0d8b4af1210a7ff41cbdca89fa93f64f82006262
Jomar Leaño <69794504+therealmai@users.noreply.github.com>
2022-12-23 20:17:15 +0800

    done with quiz app

--- lib/main.dart
+++ lib/main.dart
@@ -38,9 +38,16 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   var _totalScore = 0;
 
+  void _resetQuiz(){
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   void _answerQuestion(int score) {
     _totalScore += score;
-    
+    print("Score $_totalScore");
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -60,7 +67,7 @@ class _MyAppState extends State<MyApp> {
           title: const Text('QuizHub'),
         ),
         body: _questionIndex < _questions.length ? Quiz(answerQuestion: _answerQuestion, questionIndex: _questionIndex, questions: _questions) 
-        : Result()
+        : Result(_totalScore, _resetQuiz)
         ) ,
       );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+  final VoidCallback resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+  String get resultPhrase {
+    var resultText;
+    if (resultScore <= 10) {
+      resultText = "You don't know me";
+    } else if (resultScore <= 20 && resultScore >= 11) {
+      resultText = "You know me";
+    } else if (resultScore <= 30 && resultScore >= 21) {
+      resultText = "Stalker";
+    } else {
+      resultText = "You did it";
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You are done'),
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            width: double.infinity,
+            child: ElevatedButton(
+            child: Text("Reset Quiz"),
+            onPressed: resetHandler,
+          ))
+        ],
+      ),
     );
   }
 }
